7619a674f3b99bc89b6296e8e71f648868e1ee89
melonMochiRoll <soulhwasin@gmail.com>
2023-06-30 16:25:03 +0900

    Path Alias Setting

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -11,6 +11,12 @@ module.exports = {
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      Components: path.resolve(__dirname, 'src/components/'),
+      Containers: path.resolve(__dirname, 'src/containers/'),
+      Pages: path.resolve(__dirname, 'src/pages/'),
+      Styles: path.resolve(__dirname, 'src/styles/'),
+    },
   },
   module: {
     rules: [
