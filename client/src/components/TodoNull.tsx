8d6f8202cbd968b4da07459e5b56a4bf454c384b
melonMochiRoll <soulhwasin@gmail.com>
2023-07-28 21:24:51 +0900

    Add Component TodoNull

---
+++ client/src/components/TodoNull.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from 'react';
+import styled from '@emotion/styled';
+
+const TodoNull: FC = () => {
+  return (
+    <Text>리스트가 비어있습니다.</Text>
+  );
+};
+
+export default TodoNull;
+
+const Text = styled.span`
+  margin-top: 150px;
+  text-align: center;
+  font-size: 16px;
+  font-weight: 600;
+  color: #9298a1;
+`;
