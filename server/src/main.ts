32ce895ddf36cd12ca37f9c6f0c8d6597292fb17
melonMochiRoll <soulhwasin@gmail.com>
2023-10-10 20:00:45 +0900

    Set passport and session

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,18 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import session from 'express-session';
+import 'dotenv/config'
+import passport from 'passport';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: console,
+  });
 
-  const corsOption = {
+  const devCorsOption = {
     origin: 'http://localhost:9000',
-    methods: ['GET'],
+    methods: ['GET', 'POST'],
     optionsSuccessStatus: 204,
   };
-  app.enableCors(corsOption);
+  app.enableCors(devCorsOption);
 
-  const port = 3000;
+  const sessionOption = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: true },
+  };
+  app.use(session(sessionOption));
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  const port = Number(process.env.PORT);
   await app.listen(port);
-  console.log(`PORT :: ${port} server already`);
+  console.log(`PORT :: ${port} server is ready`);
 }
+
 bootstrap();
