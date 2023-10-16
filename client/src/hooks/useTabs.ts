4cfeb4fe6b9173755a197be1ebe48df7f29b7213
melonMochiRoll <soulhwasin@gmail.com>
2023-10-16 20:58:31 +0900

    Add useTabs

---
+++ client/src/hooks/useTabs.ts
@@ -0,0 +1,19 @@
+import { useCallback, useState } from "react";
+
+type UseTabsReturnType<T> = [
+  T,
+  (e: any, newValue: T) => void,
+  React.Dispatch<React.SetStateAction<T>>,
+];
+
+const useTabs = <T>(initData: T): UseTabsReturnType<T> => {
+  const [ state, setState ] = useState(initData);
+  
+  const onChangeState = useCallback((e: any, newValue: T) => {
+    setState(newValue);
+  }, [state]);
+
+  return [ state, onChangeState, setState ];
+};
+
+export default useTabs;
