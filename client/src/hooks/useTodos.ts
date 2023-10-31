c911fd72c98503a463df1cd5c066eae53fb30321
melonMochiRoll <soulhwasin@gmail.com>
2023-10-31 20:26:56 +0900

    Modify queryFn

--- client/src/hooks/useTodos.ts
+++ client/src/hooks/useTodos.ts
@@ -1,5 +1,5 @@
-import axios from 'axios';
 import { useQuery } from '@tanstack/react-query';
+import { getCurrentMonthTodos } from 'Api/todosApi';
 
 export type todoType = {
   id: number;
@@ -14,21 +14,13 @@ type UseTodosReturnType = [
   currentMonthTodosType,
 ];
 
-const getCurrentMonthTodosFn = async (
-  year: number,
-  monthIndex: number,
-  ) => 
-  await axios
-    .get(`http://localhost:3000/api/todos?y=${year}&mi=${monthIndex}`)
-    .then(res => res.data);
-
 const useTodos = (
   year: number,
   monthIndex: number,
   ): UseTodosReturnType => {
   const { data: currentMonthTodos } = useQuery({
     queryKey: ['getCurrentMonthTodos'],
-    queryFn: () => getCurrentMonthTodosFn(year, monthIndex),
+    queryFn: () => getCurrentMonthTodos(year, monthIndex),
     refetchOnWindowFocus: false,
   });
   
