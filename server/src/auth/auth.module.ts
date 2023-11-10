d74c1e4c4ac9bab013a370d1455514c872ab7beb
melonMochiRoll <soulhwasin@gmail.com>
2023-11-10 20:59:17 +0900

    Add providers LocalStratey, Serializer

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,14 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { UsersModule } from 'src/users/users.module';
 import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from 'src/users/users.module';
+import { AuthController } from './auth.controller';
+import { LocalStrategy } from './local.strategy';
+import { SessionSerializer } from './session.serializer';
 
 @Module({
   imports: [
-    PassportModule,
+    PassportModule.register({ session: true }),
     UsersModule,
   ],
-  providers: [ AuthService ],
+  controllers: [ AuthController ],
+  providers: [
+    AuthService,
+    LocalStrategy,
+    SessionSerializer,
+  ],
 })
 
 export class AuthModule {}
