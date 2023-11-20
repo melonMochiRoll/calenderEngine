b3fb055a63e1e663d01bbf5ef7b7be5500f347b7
melonMochiRoll <soulhwasin@gmail.com>
2023-11-21 00:09:15 +0900

    Fix withoutpassword error

--- server/src/common/decorator/user.decorator.ts
+++ server/src/common/decorator/user.decorator.ts
@@ -3,7 +3,6 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const User = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
-    const { password, ...rest } = request.user;
-    return rest;
+    return request.user;
   },
 );
