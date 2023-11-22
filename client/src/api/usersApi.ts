34482f07bbff4001f89aecdd50b7b32c23cea6a4
melonMochiRoll <soulhwasin@gmail.com>
2023-11-22 23:40:03 +0900

    Add usersApi.ts

---
+++ client/src/api/usersApi.ts
@@ -0,0 +1,13 @@
+import { axiosInstance } from "./axiosInstance";
+
+export const getUser = async () => {
+  try {
+    const { data } = await axiosInstance
+      .get('/api/users');
+    
+    return data;
+  } catch (err) {
+    console.error(err);
+    return err;
+  }
+};
