a324aad8c98afeab69d61c40501c699b8649d83b
melonMochiRoll <soulhwasin@gmail.com>
2023-11-27 22:47:24 +0900

    Add MenuButtont.tsx

---
+++ client/src/components/common/MenuButton.tsx
@@ -0,0 +1,47 @@
+import React, { FC } from 'react';
+import styled from '@emotion/styled';
+
+interface MenuButtonProps {
+  type: 'submit' | 'reset' | 'button' | undefined;
+  children: React.ReactNode;
+  onClick?: () => void;
+  filled?: boolean;
+};
+
+const MenuButton: FC<MenuButtonProps> = ({
+  type,
+  onClick,
+  children,
+  filled = false,
+}) => {
+  return (
+    <Button
+      onClick={onClick}
+      type={type}
+      filled={filled}>
+      {children}
+    </Button>
+  );
+};
+
+export default MenuButton;
+
+const Button = styled.button<{ filled: boolean }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100px;
+  height: 40px;
+  font-size: 14px;
+  font-weight: 600;
+  color: #fff;
+  background-color: ${({filled}) => filled ? `rgba(0, 0, 0, 0)` : `#6c5dd3`};
+  border: ${({filled}) => filled ? `none` : `2px solid #6c5dd3`} ;
+  border-radius: 10px;
+  cursor: pointer;
+  transition: all 0.3s;
+
+  &:hover {
+    background-color: ${({filled}) => filled ? `#6c5dd3` : `rgba(0, 0, 0, 0)`};
+  }
+`;
