636d2c59e05db81b83a69cccb1f05bdccd3ee7f2
melonMochiRoll <soulhwasin@gmail.com>
2023-11-27 22:50:09 +0900

    Add MainPage.tsx

---
+++ client/src/pages/MainPage.tsx
@@ -0,0 +1,52 @@
+import React, { FC, useState } from 'react';
+import styled from '@emotion/styled';
+import CalendarContainer from 'Containers/CalendarContainer';
+import TodoContainer from 'Containers/TodoContainer';
+import useTodos from 'Hooks/useTodos';
+import dayjs from 'dayjs';
+import Header from 'Containers/Header';
+
+const MainPage: FC = () => {
+  const [ now, setNow ] = useState(dayjs());
+  const [ currentTime, setCurrentTime ] = useState('');
+  const [ currentMonthTodos ] = useTodos(now.year(), now.month());
+
+  return (
+    <Block>
+      <CalendarBlock>
+        <Box>
+          <Header />
+          <CalendarContainer
+            now={now}
+            setNow={setNow}
+            setCurrentTime={setCurrentTime}
+            currentMonthTodos={currentMonthTodos} />
+        </Box>
+      </CalendarBlock>
+      <TodoContainer
+        currentTime={currentTime}
+        currentMonthTodos={currentMonthTodos} />
+    </Block>
+  );
+};
+
+export default MainPage;
+
+const Block = styled.div`
+  display: flex;
+`;
+
+const CalendarBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: stretch;
+  width: 100%;
+  height: 100vh;
+  padding: 30px;
+  background-color: #1f2128;
+`;
+
+const Box = styled.div`
+  width: 800px;
+`;
