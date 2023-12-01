913c02c925283e830b0727b4993d7030ce384f84
melonMochiRoll <soulhwasin@gmail.com>
2023-12-02 01:08:10 +0900

    Fix refetch condition

--- client/src/hooks/useTodos.ts
+++ client/src/hooks/useTodos.ts
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import { getCurrentMonthTodos } from 'Api/todosApi';
+import { useEffect, useState } from 'react';
 
 export type todoType = {
   id: number;
@@ -18,11 +19,15 @@ const useTodos = (
   year: number,
   monthIndex: number,
   ): UseTodosReturnType => {
-  const { data: currentMonthTodos } = useQuery<currentMonthTodosType>({
+  const { data: currentMonthTodos, refetch } = useQuery<currentMonthTodosType>({
     queryKey: ['getCurrentMonthTodos'],
     queryFn: () => getCurrentMonthTodos(year, monthIndex),
     refetchOnWindowFocus: false,
   });
+
+  useEffect(() => {
+    refetch();
+  }, [year, monthIndex]);
 
   if (!currentMonthTodos) {
     return [ {} ];
