1e278d74e81471f1ee964415bae724189ba1e48f
melonMochiRoll <soulhwasin@gmail.com>
2023-12-11 20:40:49 +0900

    Apply caching to deserialize

--- server/src/auth/session.serializer.ts
+++ server/src/auth/session.serializer.ts
@@ -1,13 +1,17 @@
-import { Injectable } from "@nestjs/common";
+import { CACHE_MANAGER } from "@nestjs/cache-manager";
+import { Inject, Injectable } from "@nestjs/common";
 import { PassportSerializer } from "@nestjs/passport";
 import { Users } from "src/entities/Users";
 import { UserWithoutPassword } from "src/typings/types";
 import { UsersService } from "src/users/users.service";
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class SessionSerializer extends PassportSerializer {
   constructor(
     private readonly usersService: UsersService,
+    @Inject(CACHE_MANAGER)
+    private cacheManager: Cache,
   ) {
     super();
   }
@@ -17,8 +21,15 @@ export class SessionSerializer extends PassportSerializer {
   };
 
   async deserializeUser(userId: number, done: (err: Error, user: UserWithoutPassword) => void) {
+    const cached = await this.cacheManager.get(`${userId}`);
+    if (cached) {
+      return done(null, cached as UserWithoutPassword);
+    }
+
     const result = await this.usersService.getOneById(userId);
     const { password, ...rest } = result;
+
+    await this.cacheManager.set(`${userId}`, rest);
 
     return result ? done(null, rest) : done(null, null);
   };
