4f74194d5827e63e163034f7020d85080c36d47e
melonMochiRoll <soulhwasin@gmail.com>
2023-12-12 20:48:46 +0900

    Fix error throw

--- client/src/api/usersApi.ts
+++ client/src/api/usersApi.ts
@@ -6,57 +6,49 @@ export const getUser = async () => {
       .get('/api/users');
     
     return data;
-  } catch (err) {
-    if (err instanceof Error) {
-      throw new Error(err.message);
-    }
+  } catch (err: any) {
+    throw new Error(err);
   }
 };
 
 export const getOneByEmail = async (email: string) => {
   try {
-    const { data } = await axiosInstance.get(`/api/users/email?e=${email}`);
+    const { data } = await axiosInstance
+      .get(`/api/users/email?e=${email}`);
 
     if (data) {
       return true;
     }
 
     return false;
-  } catch (err) {
-    if (err instanceof Error) {
-      throw new Error(err.message);
-    }
+  } catch (err: any) {
+    throw new Error(err);
   }
 };
 
 export const createUser = async (email: string, password: string) => {
   try {
-    await axiosInstance.post('/api/users', { email, password });
-  } catch (err) {
-    if (err instanceof Error) {
-      throw new Error(err.message);
-    }
+    await axiosInstance
+      .post('/api/users', { email, password });
+  } catch (err: any) {
+    throw new Error(err);
   }
 };
 
 export const login = async (email: string, password: string) => {
   try {
-    await axiosInstance.post('/api/auth/login', { username: email, password });
-    
-    return true;
-  } catch (err) {
-    if (err instanceof Error) {
-      throw new Error(err.message);
-    }
+    await axiosInstance
+      .post('/api/auth/login', { username: email, password });
+  } catch (err: any) {
+    throw new Error(err);
   }
 };
 
 export const logout = async () => {
   try {
-    await axiosInstance.post('api/auth/logout');
-  } catch (err) {
-    if (err instanceof Error) {
-      throw new Error(err.message);
-    }
+    await axiosInstance
+      .post('api/auth/logout');
+  } catch (err: any) {
+    throw new Error(err);
   }
 };
