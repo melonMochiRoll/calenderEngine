950920a7b07a797c3070a0f6d9326906ef424dd3
melonMochiRoll <soulhwasin@gmail.com>
2023-12-12 20:17:20 +0900

    Rename type

--- server/src/typings/types.ts
+++ server/src/typings/types.ts
@@ -1,6 +1,6 @@
 import { Todos } from "src/entities/Todos";
 import { Users } from "src/entities/Users";
 
-export type UsersWithoutPassword = Pick<Users, 'id' | 'email' | 'createdAt' | 'deletedAt'>;
+export type UserWithoutPassword = Pick<Users, 'id' | 'email' | 'createdAt' | 'deletedAt'>;
 
 export type todosWithoutUserId = Pick<Todos, 'id' | 'contents' | 'date'>;
