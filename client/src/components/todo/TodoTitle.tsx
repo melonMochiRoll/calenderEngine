4ffb44f375317be64ea09e81454574dd91267af5
melonMochiRoll <soulhwasin@gmail.com>
2023-12-26 15:26:57 +0900

    :lipstick: Add TodoTitle

---
+++ client/src/components/todo/TodoTitle.tsx
@@ -0,0 +1,24 @@
+import React, { FC } from 'react';
+import styled from '@emotion/styled';
+
+interface TodoTitleProps {
+  todoTime: string;
+};
+
+const TodoTitle: FC<TodoTitleProps> = ({
+  todoTime,
+}) => {
+  const [ year, month, date ] = todoTime.split('-').map(Number);
+  
+  return (
+    <Title>{`${year}년 ${month}월 ${date}일`}</Title>
+  );
+};
+
+export default TodoTitle;
+
+export const Title = styled.h1`
+  font-size: 28px;
+  font-weigth: 800;
+  color: #efeff1;
+`;
