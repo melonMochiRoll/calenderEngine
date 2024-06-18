89d75f283ef6728a7d217497b28b4b8f19eeaa10
melonMochiRoll <soulhwasin@gmail.com>
2024-06-18 18:26:03 +0900

    :lipstick: 로컬 폰트 정의

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { Global, css } from '@emotion/react';
 
 const style = css`
-  @import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css');
-
   :root {
     --black: #1f2128;
     --dark-gray: #242731;
@@ -25,6 +23,87 @@ const style = css`
 
   a {
     text-decoration: none;
+  }
+
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 900;
+    font-display: swap;
+    src:
+      url('../../public/fonts/Pretendard-Black.subset.woff2') format('woff2'),
+      url('../../public/fonts/Pretendard-Black.subset.woff') format('woff');
+  }
+
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 800;
+    font-display: swap;
+    src:
+      url('../../public/fonts/Pretendard-ExtraBold.subset.woff2') format('woff2'),
+      url('../../public/fonts/Pretendard-ExtraBold.subset.woff') format('woff');
+  }
+
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 700;
+    font-display: swap;
+    src:
+      url('../../public/fonts/Pretendard-Bold.subset.woff2') format('woff2'),
+      url('../../public/fonts/Pretendard-Bold.subset.woff') format('woff');
+  }
+
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 600;
+    font-display: swap;
+    src:
+      url('../../public/fonts/Pretendard-SemiBold.subset.woff2') format('woff2'),
+      url('../../public/fonts/Pretendard-SemiBold.subset.woff') format('woff');
+  }
+
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 500;
+    font-display: swap;
+    src:
+      url('../../public/fonts/Pretendard-Medium.subset.woff2') format('woff2'),
+      url('../../public/fonts/Pretendard-Medium.subset.woff') format('woff');
+  }
+
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 400;
+    font-display: swap;
+    src:
+      url('../../public/fonts/Pretendard-Regular.subset.woff2') format('woff2'),
+      url('../../public/fonts/Pretendard-Regular.subset.woff') format('woff');
+  }
+
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 300;
+    font-display: swap;
+    src:
+      url('../../public/fonts/Pretendard-Light.subset.woff2') format('woff2'),
+      url('../../public/fonts/Pretendard-Light.subset.woff') format('woff');
+  }
+
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 200;
+    font-display: swap;
+    src:
+      url('../../public/fonts/Pretendard-ExtraLight.subset.woff2') format('woff2'),
+      url('../../public/fonts/Pretendard-ExtraLight.subset.woff') format('woff');
+  }
+
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 100;
+    font-display: swap;
+    src:
+      url('../../public/fonts/Pretendard-Thin.subset.woff2') format('woff2'),
+      url('../../public/fonts/Pretendard-Thin.subset.woff') format('woff');
   }
 `
 
