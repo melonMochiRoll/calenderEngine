428454a5d403edfaaf6c6f3e09f29c753d777765
melonMochiRoll <soulhwasin@gmail.com>
2024-06-18 18:19:25 +0900

    :lipstick: 컬러 변수 정의

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { Global, css } from '@emotion/react';
 
-
 const style = css`
   @import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css');
+
+  :root {
+    --black: #1f2128;
+    --dark-gray: #242731;
+    --light-gray: #2f323b;
+    --pink: #bf94FF;
+    --purple: #6c5dd3;
+    --white: #efeff1;
+  }
   
   * {
     box-sizing: border-box;
