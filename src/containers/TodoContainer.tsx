2a6f9c644223d985db1d91d858097283e5e48884
melonMochiRoll <soulhwasin@gmail.com>
2024-06-24 21:30:53 +0900

    :lipstick: Todo 메뉴 열고 닫는 기능 추가

--- src/containers/TodoContainer.tsx
+++ src/containers/TodoContainer.tsx
@@ -1,7 +1,8 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import styled from '@emotion/styled';
 import TodoApp from './todo/TodoApp';
-import TodoInit from 'Components/todo/TodoInit';
+import HideIcon from '@mui/icons-material/LastPage';
+import ShowIcon from '@mui/icons-material/FirstPage';
 
 interface TodoContainerProps {
   todoTime: string;
@@ -12,26 +13,51 @@ const TodoContainer: FC<TodoContainerProps> = ({
   todoTime,
   todosListDataRefetch,
 }) => {
+  const [ isTodoShow, setIsTodoShow ] = useState(true);
+
+  const toggleMenu = () => {
+    setIsTodoShow(prev => !prev);
+  };
+  
   return (
-    <Block>
-      {todoTime ?
+    <Block isTodoShow={isTodoShow}>
+      <TodoHeader>
+        {isTodoShow ? 
+          <HideIcon
+            onClick={toggleMenu}
+            sx={{ fontSize: '30px' }} /> :
+          <ShowIcon
+            onClick={toggleMenu}
+            sx={{ fontSize: '30px' }} />}
+      </TodoHeader>
+      {isTodoShow ?
         <TodoApp
           todoTime={todoTime}
-          todosListDataRefetch={todosListDataRefetch} /> :
-        <TodoInit />
-      }
+          todosListDataRefetch={todosListDataRefetch} /> : ''}
     </Block>
   );
 };
 
 export default TodoContainer;
 
-const Block = styled.div`
+const Block = styled.div<{ isTodoShow: boolean }>`
   display: flex;
-  justify-content: center;
-  width: 550px;
+  flex-direction: column;
+  width: ${({isTodoShow}) => isTodoShow ? '550px' : '50px'};
   height: 100vh;
-  padding: 15px;
+  padding: 10px;
   border: 1px solid #2f323b;
   background-color: var(--dark-gray);
+`;
+
+const TodoHeader = styled.div`
+  svg {
+    color: var(--white);
+    border-radius: 8px;
+    cursor: pointer;
+  }
+
+  svg:hover {
+    background-color: rgba(255, 255, 255, 0.1);
+  }
 `;
