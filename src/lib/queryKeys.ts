852b151e5eb72605b995acceb9ac13c9cef760bb
melonMochiRoll <soulhwasin@gmail.com>
2024-06-26 17:49:01 +0900

    :art: querykey 모듈화

---
+++ src/lib/queryKeys.ts
@@ -0,0 +1,5 @@
+
+export const GET_USER_KEY = 'getUser';
+export const GET_TODOS_KEY = 'getTodos';
+export const GET_TODOS_LIST_KEY = 'getCurrentMonthTodosList';
+export const GET_LOCAL_TODOS_LIST_KEY = 'getLocalTodosList';
