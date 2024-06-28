06dcb2ea1742727db6a852100b2343b21a71dfc8
melonMochiRoll <soulhwasin@gmail.com>
2024-06-28 22:14:55 +0900

    :memo: GET_LOCAL_TODOS_KEY 추가

--- src/lib/queryKeys.ts
+++ src/lib/queryKeys.ts
@@ -2,4 +2,5 @@
 export const GET_USER_KEY = 'getUser';
 export const GET_TODOS_KEY = 'getTodos';
 export const GET_TODOS_LIST_KEY = 'getCurrentMonthTodosList';
+export const GET_LOCAL_TODOS_KEY = 'getLocalTodos';
 export const GET_LOCAL_TODOS_LIST_KEY = 'getLocalTodosList';
