53181a3ce28bbf008c87ab50eda80d00b253735f
melonMochiRoll <soulhwasin@gmail.com>
2024-06-28 22:15:33 +0900

    :wrench: reducer 삭제

--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import localTodosReducer from 'Features/localTodosSlice';
 
 export const reduxStore = configureStore({
   reducer: {
-    localTodos: localTodosReducer,
+
   },
 })
 
