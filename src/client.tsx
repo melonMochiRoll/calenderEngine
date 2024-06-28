4215276bc5de7f1830faf6edcffc04b1b591ad7e
melonMochiRoll <soulhwasin@gmail.com>
2024-06-28 14:34:37 +0900

    :sparkles: redux provider 적용

--- src/client.tsx
+++ src/client.tsx
@@ -4,15 +4,19 @@ import { RouterProvider } from 'react-router-dom';
 import { GlobalStyle } from 'Styles/GlobalStyle';
 import MainRouter from 'Routes/MainRouter';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Provider } from 'react-redux'
+import { reduxStore } from './store';
 
 const rootNode = document.getElementById('root') as HTMLElement;
 const queryClient = new QueryClient();
 
 createRoot(rootNode).render(
   <React.StrictMode>
+    <Provider store={reduxStore}>
     <QueryClientProvider client={queryClient}>
       <GlobalStyle />
       <RouterProvider router={MainRouter} />
     </QueryClientProvider>
+    </Provider>
   </React.StrictMode>
 );
