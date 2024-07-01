e19be5858a9311da8d2618bfbc8c0d94550f19d4
melonMochiRoll <soulhwasin@gmail.com>
2024-07-01 21:03:33 +0900

    :heavy_plus_sign: todoTimeReducer, calendarTimeReducer 추가

--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import calendarTimeReducer from 'Features/calendarTimeSlice';
+import todoTimeReducer from 'Features/todoTimeSlice';
 
 export const reduxStore = configureStore({
   reducer: {
-
+    calendarTime: calendarTimeReducer,
+    todoTime: todoTimeReducer,
   },
 })
 
