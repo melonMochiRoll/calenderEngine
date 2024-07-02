1c06ad42fbc74720617f03a40119b791a8b61024
melonMochiRoll <soulhwasin@gmail.com>
2024-07-02 17:49:16 +0900

    :wrench: headers contents-type 추가

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -4,5 +4,6 @@ const DEVELOPMENT_BASEURL = 'http://localhost:3000';
 
 export const axiosInstance = axios.create({
   baseURL: DEVELOPMENT_BASEURL,
+  headers: { "Content-Type" : "application/json" },
   withCredentials: true,
 });
