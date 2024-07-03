d342e1e06078e04972c5d7f8b5f8cb6dd7d3e752
melonMochiRoll <soulhwasin@gmail.com>
2024-07-03 23:24:51 +0900

    :sparkles: SEARCH_TODOS_KEY 추가

--- src/lib/queryKeys.ts
+++ src/lib/queryKeys.ts
@@ -4,3 +4,5 @@ export const GET_TODOS_KEY = 'getTodos';
 export const GET_TODOS_LIST_KEY = 'getCurrentMonthTodosList';
 export const GET_LOCAL_TODOS_KEY = 'getLocalTodos';
 export const GET_LOCAL_TODOS_LIST_KEY = 'getLocalTodosList';
+
+export const SEARCH_TODOS_KEY = 'searchTodos';
