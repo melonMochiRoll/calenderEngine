086cdfddd5ad06f56e027a9efc3f9b510d4dd69d
melonMochiRoll <soulhwasin@gmail.com>
2024-07-05 17:11:38 +0900

    :art: reducer 구조 수정에 따른 변경

--- src/components/RenderServerCalendar.tsx
+++ src/components/RenderServerCalendar.tsx
@@ -2,12 +2,13 @@ import React, { FC } from 'react';
 import useTodosList from 'Hooks/useTodosList';
 import CalendarCreator from './CalendarCreator';
 import { useAppSelector } from 'Hooks/reduxHooks';
+import dayjs from 'dayjs';
 
 interface RenderServerCalendarProps {}; 
 
 const RenderServerCalendar: FC<RenderServerCalendarProps> = ({}) => {
   const { calendarTime } = useAppSelector(state => state.calendarTime);
-  const [ todosListData ] = useTodosList(calendarTime.format('YYYY-MM-DD'));
+  const [ todosListData ] = useTodosList(dayjs(calendarTime).format('YYYY-MM-DD'));
   
   return (
     <CalendarCreator
