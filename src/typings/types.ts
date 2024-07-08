7fa2262f538d52477954d8a134dda1b17ab9ee67
melonMochiRoll <soulhwasin@gmail.com>
2024-07-08 17:39:24 +0900

    :label: TQueryStatus 정의

--- src/typings/types.ts
+++ src/typings/types.ts
@@ -1,6 +1,8 @@
 
 export type InputTypeAttribute = 'button' | 'checkbox' | 'color' | 'date' | 'datetime-local' | 'email' | 'file' | 'hidden' | 'image' | 'month' | 'number' | 'password' | 'radio' | 'range' | 'reset' | 'search' | 'submit' | 'tel' | 'text' | 'time' | 'url' | 'week';
 
+export type TQueryStatus = 'error' | 'success' | 'loading';
+
 export interface IUser {
   id: string;
   email: string;
