cf4c05e64c6e7002718c569c44b3cd450a190ceb
melonMochiRoll <soulhwasin@gmail.com>
2024-08-26 16:04:54 +0900

    :goal_net: 에러 로그 출력 시 해당 함수 함께 출력

--- src/api/usersApi.ts
+++ src/api/usersApi.ts
@@ -7,7 +7,7 @@ export const getUser = async () => {
     
     return data;
   } catch (err: any) {
-    console.error(err);
+    console.error(`getUser : ${err}`);
   }
 };
 
@@ -22,7 +22,8 @@ export const isUser = async (email: string) => {
 
     return false;
   } catch (err: any) {
-    console.error(err);
+    console.error(`isUser : ${err}`);
+    return false;
   }
 };
 
@@ -31,7 +32,8 @@ export const createUser = async (email: string, password: string) => {
     await axiosInstance
       .post('/api/users', { email, password });
   } catch (err: any) {
-    throw new Error();
+    console.error(`createUser : ${err}`);
+    throw new Error(err);
   }
 };
 
@@ -40,7 +42,8 @@ export const login = async (email: string, password: string) => {
     await axiosInstance
       .post('/api/auth/login', { username: email, password });
   } catch (err: any) {
-    throw new Error();
+    console.error(`login : ${err}`);
+    throw new Error(err);
   }
 };
 
@@ -49,6 +52,6 @@ export const logout = async () => {
     await axiosInstance
       .post('api/auth/logout');
   } catch (err: any) {
-    console.error(err);
+    console.error(`logout : ${err}`);
   }
 };
