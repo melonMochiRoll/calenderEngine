35e03e5648d420275dbd67a225c81b6c2e196978
melonMochiRoll <soulhwasin@gmail.com>
2024-09-24 20:22:48 +0900

    :art: GET_TODOS_LIST_KEY 수정

--- src/lib/queryKeys.ts
+++ src/lib/queryKeys.ts
@@ -1,7 +1,7 @@
 
 export const GET_USER_KEY = 'getUser';
 export const GET_TODOS_KEY = 'getTodos';
-export const GET_TODOS_LIST_KEY = 'getCurrentMonthTodosList';
+export const GET_TODOS_LIST_KEY = 'getTodosForSpace';
 export const GET_LOCAL_TODOS_KEY = 'getLocalTodos';
 export const GET_LOCAL_TODOS_LIST_KEY = 'getLocalTodosList';
 
