5988a6d8d560925509f1989266a91390fbc5530e
melonMochiRoll <soulhwasin@gmail.com>
2024-09-25 17:17:06 +0900

    :recycle: 객체로 리턴하도록 수정

--- src/hooks/useTodosList.ts
+++ src/hooks/useTodosList.ts
@@ -1,39 +1,39 @@
 import { useQuery } from '@tanstack/react-query';
-import { getCurrentMonthTodosList } from 'Api/todosApi';
+import { getTodosForSpace } from 'Api/todosApi';
 import { GET_TODOS_LIST_KEY } from 'Lib/queryKeys';
 import { useEffect } from 'react';
+import { TTodo } from 'Typings/types';
 
-export type todosListType = {
-  [todoKey: string]: {
-    partialContents: string[],
-  };
+export type TTodosList = {
+  [key: string]: TTodo[]
 };
 
-type UseTodosListReturnType = [
-  todosListType,
-  Function,
-];
+type UseTodosListReturnType = {
+  todosListData: TTodosList,
+  refetch: Function,
+}
 
 const useTodosList = (
-  date: string,
+  year: string,
+  month: string,
   ): UseTodosListReturnType => {
   const {
-    data: todosListData,
+    data,
     refetch,
   } = useQuery({
     queryKey: [GET_TODOS_LIST_KEY],
-    queryFn: () => getCurrentMonthTodosList(date),
+    queryFn: () => getTodosForSpace('D5d9a', year, month),
     refetchOnWindowFocus: false,
   });
 
   useEffect(() => {
     refetch();
-  }, [date]);
+  }, [year, month]);
 
-  return [
-    todosListData || {},
+  return {
+    todosListData: data || {},
     refetch,
-  ];
+  };
 };
 
 export default useTodosList;
