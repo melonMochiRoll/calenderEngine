9f7659ffe465ea85ad84299f6d716596078e01e3
melonMochiRoll <soulhwasin@gmail.com>
2024-09-25 16:25:06 +0900

    :fire: 로컬 기능 키 삭제

--- src/lib/queryKeys.ts
+++ src/lib/queryKeys.ts
@@ -1,8 +1,5 @@
 
 export const GET_USER_KEY = 'getUser';
 export const GET_TODOS_LIST_KEY = 'getTodosForSpace';
-export const GET_LOCAL_TODOS_KEY = 'getLocalTodos';
-export const GET_LOCAL_TODOS_LIST_KEY = 'getLocalTodosList';
 
 export const SEARCH_TODOS_KEY = 'searchTodos';
-export const SEARCH_LOCAL_TODOS_KEY = 'searchLocalTodos';
