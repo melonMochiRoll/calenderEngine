5380480be8ec05934ca9efdd3b66e330e1ef42a3
melonMochiRoll <soulhwasin@gmail.com>
2024-10-01 20:41:45 +0900

    :sparkles: SharedspacesPage 정의

---
+++ src/pages/SharedspacesPage.tsx
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react';
+import { Outlet, useMatches, useNavigate } from 'react-router-dom';
+
+const SharedspacesPage = () => {
+  const navigate = useNavigate();
+  const matches = useMatches();
+  
+  useEffect(() => {
+    if (matches.length < 2) {
+      navigate('/sharedspaces/subscribed');
+    }
+  }, []);
+  
+  return <Outlet />;
+};
+
+export default SharedspacesPage;
