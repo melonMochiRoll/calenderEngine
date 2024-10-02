9e2b6e3c3e9a178c185061f9b527843e077117a5
melonMochiRoll <soulhwasin@gmail.com>
2024-10-02 18:20:29 +0900

    :sparkles: SubscribedSpaces 페이지 정의

---
+++ src/pages/SubscribedSpacesPage.tsx
@@ -0,0 +1,33 @@
+import React, { FC, useEffect } from 'react';
+import styled from '@emotion/styled';
+import Header from 'Containers/Header';
+import SubscribedSpacesContainer from 'Containers/SubscribedSpacesContainer';
+import { useNavigate } from 'react-router-dom';
+import useUser from 'Hooks/useUser';
+
+const SubscribedSpacesPage: FC = () => {
+  const navigate = useNavigate();
+  const { userData, isLoading } = useUser();
+
+  useEffect(() => {
+    if (!isLoading && !userData) {
+      navigate('/login');
+    }
+  }, [userData]);
+  
+  return (
+    <Block>
+      <Header />
+      <SubscribedSpacesContainer />
+    </Block>
+  );
+};
+
+export default SubscribedSpacesPage;
+
+const Block = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  background-color: var(--black);
+`;
