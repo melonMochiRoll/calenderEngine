3394739f08f78eec34105c61b9cf742a683ef354
melonMochiRoll <soulhwasin@gmail.com>
2024-10-04 20:14:21 +0900

    :pencil2: GET_SUBSCRIBED_SPACES_KEY 오타 수정

--- src/lib/queryKeys.ts
+++ src/lib/queryKeys.ts
@@ -1,6 +1,6 @@
 
 export const GET_USER_KEY = 'getUser';
 export const GET_TODOS_LIST_KEY = 'getTodosForSpace';
-export const GET_SUBSCRIBED_SPACES_KEY = 'getSubscribedSpaces';
+export const GET_SUBSCRIBED_SPACES_KEY = 'getSubscribedspaces';
 
 export const SEARCH_TODOS_KEY = 'searchTodos';
