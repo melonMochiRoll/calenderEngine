2e9a3085a08e197f9b39b975bd6aad59806ba3a0
melonMochiRoll <soulhwasin@gmail.com>
2024-10-04 19:48:26 +0900

    :sparkles: GET_SUBSCRIBED_SPACES_KEY 정의

--- src/lib/queryKeys.ts
+++ src/lib/queryKeys.ts
@@ -1,5 +1,6 @@
 
 export const GET_USER_KEY = 'getUser';
 export const GET_TODOS_LIST_KEY = 'getTodosForSpace';
+export const GET_SUBSCRIBED_SPACES_KEY = 'getSharedspaces';
 
 export const SEARCH_TODOS_KEY = 'searchTodos';
