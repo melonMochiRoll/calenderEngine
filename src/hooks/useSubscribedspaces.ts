2ac18b22d20db3d5b726b03152a0531bbc130c83
melonMochiRoll <soulhwasin@gmail.com>
2024-10-06 19:38:13 +0900

    :sparkles: useSubscribedspace 정의

---
+++ src/hooks/useSubscribedspaces.ts
@@ -0,0 +1,35 @@
+import { useQuery } from "@tanstack/react-query";
+import { getSubscribedspaces } from "Api/todosApi";
+import { GET_SUBSCRIBED_SPACES_KEY } from "Lib/queryKeys";
+import { useEffect } from "react";
+import { TSubscribedspaces } from "Typings/types";
+
+type TUseSubscribedspaceReturnType = {
+  data: TSubscribedspaces[],
+  isLoading: boolean,
+  refetch: () => void,
+};
+
+const useSubscribedspace = (filter: string): TUseSubscribedspaceReturnType => {
+  const {
+    data,
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: [GET_SUBSCRIBED_SPACES_KEY],
+    queryFn: () => getSubscribedspaces(filter),
+    refetchOnWindowFocus: false,
+  });
+
+  useEffect(() => {
+    refetch();
+  }, [filter]);
+
+  return {
+    data,
+    isLoading,
+    refetch,
+  };
+};
+
+export default useSubscribedspace;
