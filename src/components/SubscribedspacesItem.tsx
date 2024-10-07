edc8ac6422e22de9cb6ba2946c893e9690a57641
melonMochiRoll <soulhwasin@gmail.com>
2024-10-07 20:02:13 +0900

    :truck: 이름 변경

---
+++ src/components/SubscribedspacesItem.tsx
@@ -0,0 +1,142 @@
+import React, { FC } from 'react';
+import styled from '@emotion/styled';
+import LockIcon from '@mui/icons-material/Lock';
+import UnlockIcon from '@mui/icons-material/LockOpen';
+import { useNavigate } from 'react-router-dom';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { Menu, MenuItem } from '@mui/material';
+import useMenu from 'Hooks/useMenu';
+import DeleteIcon from '@mui/icons-material/DeleteForeverOutlined';
+import { deleteSharedspace } from 'Api/sharedspacesApi';
+import { TSubscribedspaces } from 'Typings/types';
+
+interface TSubscribedspacesItemProps {
+  space: TSubscribedspaces,
+};
+
+const SubscribedspacesItem: FC<TSubscribedspacesItemProps> = ({
+  space,
+}) => {
+  const navigate = useNavigate();
+  const { SharedspaceId, Sharedspace } = space;
+  const { name, url, private: privateBool, Owner } = Sharedspace;
+
+  const {
+    anchorEl,
+    open,
+    onClick,
+    onClose,
+  } = useMenu();
+
+  const onClickMoreMenu = (e: any) => {
+    e.stopPropagation();
+    onClick(e);
+  };
+
+  const onCloseMoreMenu = (e: any) => {
+    e.stopPropagation();
+    onClose();
+  };
+
+  const onDeleteSharedspace = async (e: any, SharedspaceId: number) => {
+    e.stopPropagation();
+
+    await deleteSharedspace(SharedspaceId);
+    onClose();
+  };
+  
+  return (
+    <Item
+      onClick={() => navigate(`/sharedspaces/view/${url}`)}>
+      <ItemPrivate>{privateBool ? <LockIcon /> : <UnlockIcon />}</ItemPrivate>
+      <ItemTitle>{name}</ItemTitle>
+      <ItemOwner>{Owner.email}</ItemOwner>
+      <ItemMoreMenu onClick={onClickMoreMenu}>
+        <MoreVertIcon fontSize='large' />
+      </ItemMoreMenu>
+      <Menu
+        aria-labelledby='demo-positioned-button'
+        anchorEl={anchorEl}
+        open={open}
+        onClick={onCloseMoreMenu}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+        sx={{ marginTop: '10px' }}>
+          <MenuItem
+            onClick={(e) => onDeleteSharedspace(e, SharedspaceId)}
+            sx={{ gap: '5px' }}>
+            <DeleteIcon />
+            <span>삭제</span>
+          </MenuItem>
+        </Menu>
+    </Item>
+  );
+};
+
+export default SubscribedspacesItem;
+
+const Item = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  padding: 20px 30px;
+  color: var(--white);
+  border-bottom: 1px solid var(--light-gray);
+  transition: all 0.3s;
+  cursor: pointer;
+
+  &:hover {
+    background-color: var(--light-gray);
+  }
+`;
+
+const ItemPrivate = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 10%;
+  font-size: 20px;
+  text-align: center;
+  gap: 5px;
+`;
+
+const ItemTitle = styled.div`
+  width: 45%;
+  margin: 0;
+  font-size: 28px;
+`;
+
+const ItemOwner = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 25%;
+  font-size: 20px;
+  text-align: center;
+  cursor: pointer;
+`;
+
+const ItemMoreMenu = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 5%;
+  border-radius: 25px;
+  cursor: pointer;
+  transition: all 0.3s;
+
+  svg {
+    border-radius: 25px;
+
+    &:hover {
+      background-color: rgba(255, 255, 255, 0.2);
+    }
+  }
+`;
