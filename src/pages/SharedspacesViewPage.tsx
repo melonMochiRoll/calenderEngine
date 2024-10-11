6dca14688536d50dd8466a8ed5298d6819b88333
melonMochiRoll <soulhwasin@gmail.com>
2024-10-11 18:44:28 +0900

    :art: SharedspaceHeader로 헤더 변경

--- src/pages/SharedspacesViewPage.tsx
+++ src/pages/SharedspacesViewPage.tsx
@@ -1,14 +1,36 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import styled from '@emotion/styled';
 import CalendarContainer from 'Containers/CalendarContainer';
 import TodoContainer from 'Containers/TodoContainer';
 import RenderModal from 'Components/common/RenderModal';
-import Header from 'Containers/Header';
+import SharedspaceHeader from 'Containers/SharedspaceHeader';
+import useSharedspace from 'Hooks/useSharedspace';
+import { useNavigate, useParams } from 'react-router-dom';
+
+// Header
+// TODO: 스페이스 제목 더블 클릭시 제목 수정 기능 추가
+// TODO: 스페이스에 대한 권한이 없을시 대응 추가
 
 const SharedspacesViewPage: FC = () => {
+  const { url = '' } = useParams();
+  const navigate = useNavigate();
+
+  const {
+    data: spaceData,
+    isLoading,
+  } = useSharedspace(url);
+
+  useEffect(() => {
+    if (!isLoading && !spaceData) {
+      navigate('/not-found');
+    }
+  }, [isLoading, spaceData]);
+  
   return (
     <Block>
-      <Header />
+      <SharedspaceHeader
+        spaceData={spaceData}
+        isLoading={isLoading} />
       <Main>
         <CalendarContainer />
         <TodoContainer />
