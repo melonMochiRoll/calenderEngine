2a67be791d893481e4bc293f6efdbc622149dca5
melonMochiRoll <soulhwasin@gmail.com>
2024-10-11 18:48:14 +0900

    :art: 아이콘 추가

--- src/containers/Header.tsx
+++ src/containers/Header.tsx
@@ -1,13 +1,24 @@
 import React, { FC } from 'react';
 import styled from '@emotion/styled';
 import RenderUserProfile from 'Components/auth/RenderUserProfile';
+import SatelliteIcon from '@mui/icons-material/SatelliteAlt';
+import { useNavigate } from 'react-router-dom';
 
 interface HeaderProps {};
 
-const Header: FC<HeaderProps> = () => {
+const Header: FC<HeaderProps> = ({}) => {
+  const navigate = useNavigate();
+  
   return (
     <Block>
-      <Left></Left>
+      <Left>
+        <FlexBox>
+          <SatelliteIcon
+            onClick={() => navigate('/sharedspaces/subscribed')}
+            fontSize='large'
+            sx={{ color: 'var(--blue)', cursor: 'pointer', marginRight: '10px' }}/>
+        </FlexBox>
+      </Left>
       <Right>
         <RenderUserProfile />
       </Right>
@@ -28,11 +39,21 @@ const Block = styled.header`
 
 const Left = styled.div`
   display: flex;
+  justify-content: space-between;
   align-items: center;
+  width: 70%;
+`;
+
+const FlexBox = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
 `;
 
 const Right = styled.div`
   display: flex;
+  justify-content: flex-end;
   align-items: center;
+  width: 30%;
   gap: 12px;
 `;
