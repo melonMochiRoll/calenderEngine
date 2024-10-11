13c710c3bde64668734167bcf27d5c7bf1129226
melonMochiRoll <soulhwasin@gmail.com>
2024-10-11 18:45:53 +0900

    :sparkles: GET_SHAREDSPACE_KEY 정의

--- src/lib/queryKeys.ts
+++ src/lib/queryKeys.ts
@@ -1,6 +1,7 @@
 
 export const GET_USER_KEY = 'getUser';
 export const GET_TODOS_LIST_KEY = 'getTodosForSpace';
+export const GET_SHAREDSPACE_KEY = 'getSharedspace';
 export const GET_SUBSCRIBED_SPACES_KEY = 'getSubscribedspaces';
 
 export const SEARCH_TODOS_KEY = 'searchTodos';
