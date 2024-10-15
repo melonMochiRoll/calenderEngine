1ef9ea24de76a2332b86c4dfcb4dfcc77aa902c9
melonMochiRoll <soulhwasin@gmail.com>
2024-10-15 19:54:13 +0900

    :sparkles: waitingMessage 정의

--- src/lib/noticeConstants.ts
+++ src/lib/noticeConstants.ts
@@ -4,3 +4,5 @@ export const privateTooltip = '비공개 스페이스는 멤버로 초대받은 
 export const emptyspaces = '스페이스가 없습니다.';
 
 export const forbiddenErrorMessage = '접근 권한이 없습니다.';
+
+export const waitingMessage = '잠시후에 다시 시도해주세요.';
