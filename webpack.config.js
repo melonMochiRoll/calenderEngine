066b92519cb4c6e098984a8648df931f2de7b7ed
melonMochiRoll <soulhwasin@gmail.com>
2024-10-15 19:46:15 +0900

    :wrench: style-loader, css-loader 추가

--- webpack.config.js
+++ webpack.config.js
@@ -49,6 +49,10 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
   devServer: {
