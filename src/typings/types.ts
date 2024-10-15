46ffcccf8f079c8e0250c21663a2ab8f7ae5c4fa
melonMochiRoll <soulhwasin@gmail.com>
2024-10-15 18:17:08 +0900

    :sparkles: TErrorResponse 정의

--- src/typings/types.ts
+++ src/typings/types.ts
@@ -1,6 +1,14 @@
 
 export type InputTypeAttribute = 'button' | 'checkbox' | 'color' | 'date' | 'datetime-local' | 'email' | 'file' | 'hidden' | 'image' | 'month' | 'number' | 'password' | 'radio' | 'range' | 'reset' | 'search' | 'submit' | 'tel' | 'text' | 'time' | 'url' | 'week';
 
+export type TErrorResponse = {
+  error: string,
+  message: string,
+  path: string,
+  statusCode: number,
+  timestamp: string,
+};
+
 export type TQueryStatus = 'error' | 'success' | 'loading';
 
 export const SharedspaceMembersRoles = {
