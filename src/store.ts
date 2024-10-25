243b6dcc55434b098814ce88f72ec32c2dd32609
melonMochiRoll <soulhwasin@gmail.com>
2024-10-25 17:26:59 +0900

    :heavy_plus_sign: searchUsersReducer 의존성 추가

--- src/store.ts
+++ src/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import modalReducer from 'Features/modalSlice';
 import calendarTimeReducer from 'Features/calendarTimeSlice';
 import todoTimeReducer from 'Features/todoTimeSlice';
+import searchUsersReducer from 'Features/searchUsersSlice';
 
 export const reduxStore = configureStore({
   reducer: {
     modal: modalReducer,
     calendarTime: calendarTimeReducer,
     todoTime: todoTimeReducer,
+    searchUsers: searchUsersReducer,
   },
 })
 
