00ab3827286608556d0515d9e980106d43cba15d
melonMochiRoll <soulhwasin@gmail.com>
2024-10-28 19:34:16 +0900

    :sparkles: handleHooksError 정의

---
+++ src/lib/handleHooksError.ts
@@ -0,0 +1,35 @@
+import { AxiosError } from "axios";
+import { toast } from "react-toastify";
+import { defaultToastOption, waitingMessage } from "./noticeConstants";
+import { NavigateFunction } from "react-router-dom";
+
+const handleHooksError = (error: unknown, navigate: NavigateFunction) => {
+  if (error instanceof AxiosError) {
+    const { statusCode, message } = error.response?.data;
+
+    toast.error(message, {
+      ...defaultToastOption,
+    });
+
+    if (statusCode >= 500) {
+      return navigate('internal');
+    }
+    
+    if (statusCode === 404) {
+      return navigate('not-found');
+    }
+
+    if (statusCode === 403) {
+      return navigate('forbidden');
+    }
+
+    return navigate('/');
+  }
+
+  toast.error(waitingMessage, {
+    ...defaultToastOption,
+  });
+  navigate('internal');
+};
+
+export default handleHooksError;
