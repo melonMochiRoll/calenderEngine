1ddb04107b69c9c21294ffc9cfb6a7f29f112b22
melonMochiRoll <soulhwasin@gmail.com>
2024-10-29 21:59:03 +0900

    :sparkles: checkContent 정의

--- src/lib/noticeConstants.ts
+++ src/lib/noticeConstants.ts
@@ -4,6 +4,8 @@ export const privateTooltip = '비공개 스페이스는 멤버로 초대받은 
 
 export const emptyspaces = '스페이스가 없습니다.';
 
+export const checkContent = '내용을 확인해주세요.';
+
 export const forbiddenErrorMessage = '접근 권한이 없습니다.';
 
 export const waitingMessage = '잠시후에 다시 시도해주세요.';
