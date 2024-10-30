6c7a9c0752eee032f53252587846e304d9fbed5c
melonMochiRoll <soulhwasin@gmail.com>
2024-10-30 18:55:28 +0900

    :label: state, Payload 타입 정의

--- src/features/subscribedspacesFilterSlice.ts
+++ src/features/subscribedspacesFilterSlice.ts
@@ -1,14 +1,15 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { SubscribedspacesFilter, TSubscribedspacesFilter } from "Typings/types";
 
-const initialState = {
-  filter: '',
+const initialState: { filter: TSubscribedspacesFilter } = {
+  filter: SubscribedspacesFilter.ALL,
 };
 
 export const subscribedspacesFilterSlice = createSlice({
   name: 'subscribedspacesFilter',
   initialState,
   reducers: {
-    setFilter: (state, actions: PayloadAction<{ filter: string }>) => {
+    setFilter: (state, actions: PayloadAction<{ filter: TSubscribedspacesFilter }>) => {
       state.filter = actions.payload.filter;
     },
   },
