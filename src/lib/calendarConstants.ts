c1a48ff8c6914a82e0d6b7c39f7477013762bc25
melonMochiRoll <soulhwasin@gmail.com>
2024-11-04 16:15:51 +0900

    :sparkles: TODO_PALETTE 정의

--- src/lib/calendarConstants.ts
+++ src/lib/calendarConstants.ts
@@ -2,3 +2,7 @@
 export const DAYS = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
 
 export const TODO_MAX_SIZE = 65533;
+
+export const TODO_PALETTE = [
+  '#F79552', '#F2728C', '#03c75a', '#4EB8B9', '#27AAE1', '#9E7EB9', '#EF404A',
+];
