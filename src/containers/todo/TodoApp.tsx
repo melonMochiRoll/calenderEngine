aed690fba9fb063863836844b88eab8e50fcf452
melonMochiRoll <soulhwasin@gmail.com>
2024-11-05 16:48:22 +0900

    :art: TodoInput 모달 형태로 수정

--- src/containers/todo/TodoApp.tsx
+++ src/containers/todo/TodoApp.tsx
@@ -1,74 +1,32 @@
 import React, { FC } from 'react';
 import styled from '@emotion/styled';
 import TodoList from 'Components/todo/TodoList';
-import TodoInput from 'Components/todo/TodoInput';
-import { useQueryClient } from '@tanstack/react-query';
-import { createDateTodos, deleteDateTodos, updateDateTodos } from 'Api/todosApi';
 import TodoTitle from 'Components/todo/TodoTitle';
-import useTodos from 'Hooks/useTodos';
+import { useAppDispatch, useAppSelector } from 'Hooks/reduxHooks';
 import TodoInit from 'Components/todo/TodoInit';
-import { GET_TODOS_LIST_KEY } from 'Lib/queryKeys';
-import { useAppSelector } from 'Hooks/reduxHooks';
+import AddIcon from '@mui/icons-material/AddCircleOutlineRounded';
+import { openModal } from 'Features/modalSlice';
+import { ModalName } from 'Typings/types';
 
 interface TodoAppProps {};
 
 const TodoApp: FC<TodoAppProps> = ({}) => {
-  const qc = useQueryClient();
+  const dispatch = useAppDispatch();
   const { todoTime } = useAppSelector(state => state.todoTime);
-  const [ todosStatus, todosData, todosDataRefetch ] = useTodos(todoTime);
-
-  const addTodo = async (contents: string) => {
-    if (!contents || contents.length > 30) return;
-
-    await createDateTodos(
-      contents,
-      todoTime,
-    );
-    todosDataRefetch();
-    await qc.refetchQueries([GET_TODOS_LIST_KEY]);
-  };
-
-  const updateTodo = async (
-    todosId: number,
-    contents: string,
-    isComplete: boolean,
-    ) => {
-
-    await updateDateTodos(
-      todosId,
-      contents,
-      isComplete,
-      todoTime,
-    );
-    todosDataRefetch();
-    await qc.refetchQueries([GET_TODOS_LIST_KEY]);
-  };
-
-  const deleteTodo = async (todosId: number) => {
-    await deleteDateTodos(
-      todosId,
-      todoTime,
-    );
-    todosDataRefetch();
-    await qc.refetchQueries([GET_TODOS_LIST_KEY]);
-  };
-
+  
+  if (!todoTime) {
+    return <TodoInit />;
+  }
+  
   return (
     <Container>
-      {todoTime ?
-        <>
-        <TodoTitle
-          todoTime={todoTime}/>
-        <TodoInput
-          addTodo={addTodo} />
-        <TodoList
-          todosStatus={todosStatus}
-          todosData={todosData}
-          updateTodo={updateTodo}
-          deleteTodo={deleteTodo} />
-        </> :
-        <TodoInit />
-      }
+      <TodoTitle />
+      <FlexBox
+        onClick={() => dispatch(openModal(ModalName.TODO_INPUT))}>
+        <AddIcon fontSize='large' sx={{ color: 'var(--blue)' }}/>
+        <Span>새 Todo 작성</Span>
+      </FlexBox>
+      <TodoList />
     </Container>
   );
 };
@@ -81,4 +39,25 @@ const Container = styled.div`
   align-items: center;
   height: 100%;
   padding: 0 10px;
+`;
+
+const FlexBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 10px 0;
+  padding: 10px 20px;
+  color: var(--white);
+  border: 1px solid var(--gray-7);
+  cursor: pointer;
+  gap: 6px;
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
+`;
+
+const Span = styled.span`
+  font-size: 22px;
+  font-weight: 700;
 `;
