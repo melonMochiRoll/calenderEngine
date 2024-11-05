1593b070d6971b0b4cf2252e11e103790770a30f
melonMochiRoll <soulhwasin@gmail.com>
2024-11-05 16:47:53 +0900

    :sparkles: TodoInput 모달 형태로 재정의

--- src/components/modal/todo/TodoInput.tsx
+++ src/components/modal/todo/TodoInput.tsx
@@ -6,24 +6,32 @@ import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
 import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
-import { checkContent } from 'Lib/noticeConstants';
-import { createTodo } from 'Api/todosApi';
+import CloseIcon from '@mui/icons-material/CloseRounded';
+import { useAppDispatch, useAppSelector } from 'Hooks/reduxHooks';
+import { closeModal } from 'Features/modalSlice';
+import TextButton from 'Components/common/TextButton';
 import { useQueryClient } from '@tanstack/react-query';
-import { GET_TODOS_LIST_KEY } from 'Lib/queryKeys';
 import { useParams } from 'react-router-dom';
-import { useAppSelector } from 'Hooks/reduxHooks';
 import useUser from 'Hooks/useUser';
+import { createTodo } from 'Api/todosApi';
+import { GET_TODOS_LIST_KEY } from 'Lib/queryKeys';
+import { checkContent } from 'Lib/noticeConstants';
 
-interface TodoInputProps {};
+const getByteSize = (str: string) => {
+  const encoder = new TextEncoder();
 
-const TodoInput: FC<TodoInputProps> = ({}) => {
+  return encoder.encode(str).length;
+};
+
+const TodoInput: FC = () => {
   dayjs.extend(utc)
   dayjs.extend(timezone)
   dayjs.extend(customParseFormat);
   dayjs.extend(isSameOrBefore);
   const timeZone = dayjs.tz.guess();
-
   const qc = useQueryClient();
+  const dispatch = useAppDispatch();
+
   const { url = '' } = useParams();
   const { userData } = useUser();
   const { todoTime } = useAppSelector(state => state.todoTime);
@@ -106,9 +114,10 @@ const TodoInput: FC<TodoInputProps> = ({}) => {
     const dayjs_endTime = dayjs(endTimeFormat === '00:00:00' ? '24:00:00' : endTimeFormat, 'HH:mm:ss').tz(timeZone);
 
     if (
+      startTimeFormat === endTimeFormat ||
       !dayjs_startTime.isValid() ||
       !dayjs_endTime.isValid() ||
-      !dayjs(dayjs_startTime).isSameOrBefore(dayjs_endTime)
+      !dayjs(dayjs_startTime).isBefore(dayjs_endTime)
     ) {
       return setError(true);
     }
@@ -125,6 +134,7 @@ const TodoInput: FC<TodoInputProps> = ({}) => {
       setStartTime(initialTime);
       setEndTime(initialTime);
       setDescription('');
+      dispatch(closeModal());
       await qc.refetchQueries([GET_TODOS_LIST_KEY]);
     })
     .catch(() => {
@@ -133,65 +143,91 @@ const TodoInput: FC<TodoInputProps> = ({}) => {
   };
 
   return (
-    <Block>
-      <Top>
-        <TimeBox>
-          <TimeInput
-            value={startTime.hour}
-            onChange={onChangeStartTime}
-            name='hour'
-            type='text'
-            maxLength={2}
-            placeholder='00' />
-          <span>:</span>
-          <TimeInput
-            value={startTime.minute}
-            onChange={onChangeStartTime}
-            id='1'
-            name='minute'
-            type='text'
-            maxLength={2}
-            placeholder='00' />
-        </TimeBox>
-        <span>~</span>
-        <TimeBox>
-          <TimeInput
-            value={endTime.hour}
-            onChange={onChangeEndTime}
-            name='hour'
-            type='text'
-            maxLength={2}
-            placeholder='00' />
-          <span>:</span>
-          <TimeInput
-            value={endTime.minute}
-            onChange={onChangeEndTime}
-            name='minute'
-            type='text'
-            maxLength={2}
-            placeholder='00' />
-        </TimeBox>
-      </Top>
-      <Bottom>
-        <DescriptionInput
-          value={description}
-          onChange={onChangeDescriptionWithMaxSize}
-          name='description'
-          type='text'
-          placeholder='새 할일' />
-        <Button
-          onClick={() => onSubmit(
-            description,
-            todoTime,
-            startTime,
-            endTime,
-            userData?.id,
-            url)
-          }>
-          추가
-        </Button>
-      </Bottom>
-      {error && <ErrorSpan>{checkContent}</ErrorSpan>}
+    <Block
+      onClick={e => e.stopPropagation()}>
+      <Header>
+        <Left></Left>
+        <Center>
+          <ModalTitle>Todo 작성</ModalTitle>
+        </Center>
+        <Right>
+          <CloseIcon
+            onClick={() => dispatch(closeModal())}
+            sx={{
+              color: 'var(--white)',
+              fontSize: '35px',
+              cursor: 'pointer',
+            }} />
+        </Right>
+      </Header>
+      <Main>
+        <TimeDiv>
+          <TimeBox>
+            <span>시작 시간</span>
+            <TimeInput
+              value={startTime.hour}
+              onChange={onChangeStartTime}
+              name='hour'
+              type='text'
+              maxLength={2}
+              placeholder='00' />
+            <span>:</span>
+            <TimeInput
+              value={startTime.minute}
+              onChange={onChangeStartTime}
+              name='minute'
+              type='text'
+              maxLength={2}
+              placeholder='00' />
+          </TimeBox>
+          <TimeBox>
+            <span>종료 시간</span>
+            <TimeInput
+              value={endTime.hour}
+              onChange={onChangeEndTime}
+              name='hour'
+              type='text'
+              maxLength={2}
+              placeholder='00' />
+            <span>:</span>
+            <TimeInput
+              value={endTime.minute}
+              onChange={onChangeEndTime}
+              name='minute'
+              type='text'
+              maxLength={2}
+              placeholder='00' />
+          </TimeBox>
+        </TimeDiv>
+        <DescriptionDiv>
+          <DescriptionInput
+            value={description}
+            onChange={onChangeDescriptionWithMaxSize}
+            placeholder='내용' />
+        </DescriptionDiv>
+        <SubmitDiv>
+          <Left></Left>
+          <Center>
+            {error && <ErrorSpan>{checkContent}</ErrorSpan>}
+          </Center>
+          <Right>
+            <TextButton
+              type='button'
+              onClick={() => {
+                onSubmit(
+                  description,
+                  todoTime,
+                  startTime,
+                  endTime,
+                  userData?.id,
+                  url,
+                );
+              }}>
+              공유
+            </TextButton>
+          </Right>
+        </SubmitDiv>
+      </Main>
     </Block>
   );
 };
@@ -201,50 +237,77 @@ export default TodoInput;
 const Block = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  width: 100%;
-  margin-bottom: 15px;
-  gap: 8px;
+  width: 450px;
+  height: 300px;
+  border-radius: 15px;
+  background-color: var(--black);
+  box-shadow: 1px 1px 10px 2px #000;
 `;
 
-const Top = styled.div`
-  display: flex;
-  align-items: center;
-  color: var(--white);
-
-  svg {
-    cursor: pointer;
-  }
-`;
-
-const Bottom = styled.div`
+const Header = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 2px;
-  border: 1px solid var(--light-gray);
-  border-radius: 15px;
-  background-color: var(--light-gray);
+  width: 100%;
+  height: 20%;
+  padding: 20px 0;
+  border-bottom: 1px solid var(--light-gray);
+`;
 
-  svg {
-    cursor: pointer;
+const Left = styled.div`
+  width: 15%;
+`;
+
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 70%;
+`;
+
+const ModalTitle = styled.h1`
+  color: var(--white);
+  font-size: 24px;
+  font-weight 600;
+  margin: 0;
+`;
+
+const Right = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 15%;
+`;
+
+const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 80%;
+  color: var(--white);
+`;
+
+const TimeDiv = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 20%;
+  border-bottom: 1px solid var(--gray-8);
+
+  span {
+    font-size: 18px;
+    font-weight: 600;
   }
 `;
 
 const TimeBox = styled.div`
-  width: 100px;
   display: flex;
+  justify-content: center;
   align-items: center;
-
-  span {
-    font-size: 22px;
-    font-weight: 600;
-    padding-bottom: 4px;
-  }
+  width: 50%;
+  gap: 5px;
 `;
 
 const TimeInput = styled.input`
-  width: 50%;
+  width: 40px;
   font-size: 22px;
   font-weight: 500;
   text-align: center;
@@ -263,19 +326,26 @@ const TimeInput = styled.input`
   }
 `;
 
-const DescriptionInput = styled.input`
+const DescriptionDiv = styled.div`
   width: 100%;
-  padding: 12px 15px;
-  font-size: 20px;
+  height: 60%;
+  padding: 20px;
+  border-bottom: 1px solid var(--gray-8);
+`;
+
+const DescriptionInput = styled.textarea`
+  width: 100%;
+  height: 100%;
+  font-size: 18px;
   font-weight: 500;
   color: var(--white);
-  background-color: var(--light-gray);
   border: none;
-  border-radius: 15px;
+  background-color: var(--black);
+  resize: none;
 
   &::placeholder {
     font-weight: 600;
-    color: #dedee3;
+    color: var(--gray.8);
   }
 
   &:focus {
@@ -283,30 +353,15 @@ const DescriptionInput = styled.input`
   }
 `;
 
-const Button = styled.button`
-  width: 100px;
-  height: 100%;
-  font-size: 18px;
-  font-weight: 600;
-  color: #fff;
-  background-color: var(--purple);
-  cursor: pointer;
-  border: 2px solid var(--purple);
-  border-radius: 15px;
-  transition: all 0.3s;
-
-  &:hover {
-    background-color: rgba(0, 0, 0, 0);
-  }
+const SubmitDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 20%;
 `;
 
 const ErrorSpan = styled.span`
   font-size: 16px;
   color: var(--red);
 `;
-
-const getByteSize = (str: string) => {
-  const encoder = new TextEncoder();
-
-  return encoder.encode(str).length;
-};
