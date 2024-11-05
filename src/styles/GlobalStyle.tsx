a61539f17c590d2fd8ebf61c90696a7c86e54495
melonMochiRoll <soulhwasin@gmail.com>
2024-11-05 15:13:26 +0900

    :lipstick: 회색 hex 코드 정의

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -30,6 +30,16 @@ const style = css`
     --red: #f94449;
     --blue: #63bcff;
     --naver-green: #03c75a;
+    --gray-0: #f8f9fa;
+    --gray-1: #f1f3f5;
+    --gray-2: #e9ecef;
+    --gray-3: #dee2e6;
+    --gray-4: #ced4da;
+    --gray-5: #adb5bd;
+    --gray-6: #868e96;
+    --gray-7: #495057;
+    --gray-8: #343a40;
+    --gray-9: #212529;
   }
   
   * {
