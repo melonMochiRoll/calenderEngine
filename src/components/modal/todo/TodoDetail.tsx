ff375b725c1704e2bdd0c8b43479c9646d51340c
melonMochiRoll <soulhwasin@gmail.com>
2024-11-07 18:41:21 +0900

    :art: Todo 수정 및 삭제 메뉴 추가

--- src/components/modal/todo/TodoDetail.tsx
+++ src/components/modal/todo/TodoDetail.tsx
@@ -1,22 +1,81 @@
 import React, { FC } from 'react';
 import styled from '@emotion/styled';
 import { useAppDispatch, useAppSelector } from 'Hooks/reduxHooks';
-import { closeModal } from 'Features/modalSlice';
+import { closeModal, openNestedModal } from 'Features/modalSlice';
 import CloseIcon from '@mui/icons-material/CloseRounded';
 import ClockIcon from '@mui/icons-material/AccessTime';
 import DescriptionIcon from '@mui/icons-material/Description';
 import PencilIcon from '@mui/icons-material/Create';
 import dayjs from 'dayjs';
+import MenuIcon from '@mui/icons-material/MoreHoriz';
+import { NestedModalName, SharedspaceMembersRoles, TSharedspaceMembers, TUser } from 'Typings/types';
+import useMenu from 'Hooks/useMenu';
+import { Menu, MenuItem } from '@mui/material';
+import { deleteTodo } from 'Api/todosApi';
+import { useParams } from 'react-router-dom';
+import { useQueryClient } from '@tanstack/react-query';
+import { GET_TODOS_LIST_KEY } from 'Lib/queryKeys';
+import useUser from 'Hooks/useUser';
+import useSharedspace from 'Hooks/useSharedspace';
 
 const TodoDetail: FC = () => {
+  const qc = useQueryClient();
   const dispatch = useAppDispatch();
+  const { url = '' } = useParams();
   const { todo } = useAppSelector(state => state.todoDetail);
+  const { hasPermission } = useUser();
+  const { data: spaceData } = useSharedspace();
+
+  const {
+    anchorEl,
+    open,
+    onOpen,
+    onClose,
+  } = useMenu();
+
+  const onClickTodoDelete = async (todoId: number, url: string) => {
+    await deleteTodo(
+      todoId,
+      url,
+    )
+    dispatch(closeModal());
+    await qc.refetchQueries([GET_TODOS_LIST_KEY]);
+  };
   
   return (
     <Block
       onClick={e => e.stopPropagation()}>
       <Header>
-        <Left></Left>
+        <Left>
+          {hasPermission(spaceData?.id) &&
+            <MenuIcon
+              onClick={onOpen}
+              fontSize='large'
+              sx={{ color: 'var(--white)', cursor: 'pointer' }} />
+          }
+          <Menu
+            aria-labelledby='demo-positioned-button'
+            anchorEl={anchorEl}
+            open={open}
+            onClick={onClose}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'center',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'center',
+            }}>
+              <MenuItem
+                onClick={() => dispatch(openNestedModal(NestedModalName.TODO_UPDATE))}>
+                <span>수정</span>
+              </MenuItem>
+              <MenuItem
+                onClick={() => onClickTodoDelete(todo?.id as number, url)}>
+                <span>삭제</span>
+              </MenuItem>
+            </Menu>
+        </Left>
         <Center>
           <ModalTitle>Todo</ModalTitle>
         </Center>
@@ -46,7 +105,7 @@ const TodoDetail: FC = () => {
           </Content>
         </DetailDiv>
         <UpdateAtDiv>
-          {todo?.Editor && <LastupdatedAt>{`Last UpdatedAt : ${todo?.Editor.email}, ${todo?.updatedAt}`}</LastupdatedAt>}
+          {todo?.Editor && <LastupdatedAt>{`Last UpdatedAt : ${todo?.Editor.email}, ${dayjs(todo?.updatedAt).format('YYYY-MM-DD HH:mm')}`}</LastupdatedAt>}
         </UpdateAtDiv>
       </Main>
     </Block>
@@ -76,6 +135,8 @@ const Header = styled.header`
 `;
 
 const Left = styled.div`
+  display: flex;
+  justify-content: center;
   width: 15%;
 `;
 
