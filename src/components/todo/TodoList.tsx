e3858cfe0a28b50315b1e2b931f134d5f33bafcf
melonMochiRoll <soulhwasin@gmail.com>
2024-11-07 18:44:48 +0900

    :sparkles: TodoList 재정의

--- src/components/todo/TodoList.tsx
+++ src/components/todo/TodoList.tsx
@@ -1,24 +1,21 @@
-import React, { FC, memo } from 'react';
+import React, { FC, Fragment } from 'react';
 import styled from '@emotion/styled';
-import TodoItem from 'Components/todo/TodoItem';
 import TodoNull from 'Components/todo/TodoNull';
-import { TQueryStatus, TTodo } from 'Typings/types';
+import { TTodo } from 'Typings/types';
+import useTodosList from 'Hooks/useTodosList';
+import { useAppSelector } from 'Hooks/reduxHooks';
+import TodoItem from './TodoItem';
+import { TODO_PALETTE } from 'Lib/calendarConstants';
+import { getTodoHeight, renderTime } from 'Lib/utilFunction';
+import TodoBlank from './TodoBlank';
 
-interface TodoListProps {
-  todosStatus: TQueryStatus;
-  todosData: TTodo[];
-  updateTodo: (todosId: number, contents: string, isComplete: boolean) => void;
-  deleteTodo: (todosId: number) => void;
-};
+interface TodoListProps {};
 
-const TodoList: FC<TodoListProps> = ({
-  todosStatus,
-  todosData,
-  updateTodo,
-  deleteTodo,
-}) => {
+const TodoList: FC<TodoListProps> = ({}) => {
+  const { data: todosData } = useTodosList();
+  const { todoTime } = useAppSelector(state => state.todoTime);
 
-  if (!todosData || !todosData?.length || todosStatus === 'loading') {
+  if (!todosData[todoTime] || !todosData[todoTime]?.length) {
     return (
       <Block>
         <TodoNull />
@@ -27,47 +24,89 @@ const TodoList: FC<TodoListProps> = ({
   }
 
   return (
-    <>
-      <Block>
+    <Block>
+      <ListHeader>
+        <TimeDiv>
+          <TimeSpan>{renderTime(todosData[todoTime][0]?.startTime)}</TimeSpan>
+        </TimeDiv>
+      </ListHeader>
+      <ListBody>
         {
-          todosData.map((todo: TTodo, i: number) => 
-            <TodoItem
-              key={todo.id}
-              todos={todo}
-              updateTodo={updateTodo}
-              deleteTodo={deleteTodo} />
-          )
+          todosData[todoTime].map((todo: TTodo, idx: number) => {
+            const { endTime } = todo;
+            const isNotLastTodo = idx !== todosData[todoTime].length - 1;
+
+            if (isNotLastTodo) {
+              const { startTime: nextTodoStartTime } = todosData[todoTime][idx + 1];
+              const hasTimeGap = endTime !== nextTodoStartTime;
+
+              if (hasTimeGap) {
+                return (
+                  <Fragment key={todo.id}>
+                    <TodoItem
+                      todo={todo}
+                      bgColor={TODO_PALETTE[idx % 7]} />
+                    <TodoBlank
+                      blankHeight={getTodoHeight(endTime, nextTodoStartTime)}
+                      nextTodoStartTime={nextTodoStartTime}
+                      borderBottomColor={TODO_PALETTE[(idx + 1) % 7]} />
+                  </Fragment>
+                );
+              }
+            }
+
+            return (
+              <TodoItem
+                key={todo.id}
+                todo={todo}
+                bgColor={TODO_PALETTE[idx % 7]} />
+            );
+          })
         }
-      </Block>
-      <TodosCount>
-        <span>{`${todosData?.length || 0}/20`}</span>
-      </TodosCount>
-    </> 
+      </ListBody>
+    </Block>
   );
 };
 
-export default memo(TodoList);
+export default TodoList;
 
 const Block = styled.div`
   display: flex;
   width: 100%;
-  height: 100%;
-  padding: 5px;
+  height: 75%;
+  padding: 10px;
+  margin-bottom: 30px;
   flex-direction: column;
-  align-items: center;
-  border-bottom: 1px solid var(--light-gray);
   overflow: auto;
-  gap: 10px;
 `;
 
-const TodosCount = styled.div`
+const ListHeader = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  color: var(--white);
+  font-size: 22px;
+  font-weigth: 600;
+  margin: 0;
+  border-bottom: 1px solid var(--white);
+`;
+
+const TimeDiv = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: end;
-  padding: 10px;
+  justify-content: flex-end;
+  align-items: center;
+  width: 15%;
+  height: 30px;
+`;
 
-  span {
-    font-size: 20px;
-    color: #fff;
-  }
+const TimeSpan = styled.span`
+  color: var(--white);
+  font-size: 22px;
+  font-weight: 500;
+`;
+
+const ListBody = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
