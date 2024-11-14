afc14e8758282095424a98355d345cf7a17cbf15
melonMochiRoll <soulhwasin@gmail.com>
2024-11-14 17:21:57 +0900

    :sparkles: GET_MY_JOINREQUEST_KEY 정의

--- src/lib/queryKeys.ts
+++ src/lib/queryKeys.ts
@@ -3,6 +3,7 @@ export const GET_USER_KEY = 'getUser';
 export const GET_TODOS_LIST_KEY = 'getTodosForSpace';
 export const GET_SHAREDSPACE_KEY = 'getSharedspace';
 export const GET_SUBSCRIBED_SPACES_KEY = 'getSubscribedspaces';
+export const GET_MY_JOINREQUEST_KEY = 'getMyJoinRequest';
 
 export const SEARCH_TODOS_KEY = 'searchTodos';
 export const SEARCH_USERS_KEY = 'searchUsers';
