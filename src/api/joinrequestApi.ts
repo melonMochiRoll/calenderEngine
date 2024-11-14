be1c5ef7fd874e7eee4d861f9c4c0803eddd4b64
melonMochiRoll <soulhwasin@gmail.com>
2024-11-14 20:19:22 +0900

    :sparkles: getMyJoinRequest 정의

--- src/api/joinrequestApi.ts
+++ src/api/joinrequestApi.ts
@@ -1,5 +1,18 @@
 import { axiosInstance } from "./axiosInstance";
 
+export const getMyJoinRequest = async (
+  url: string,
+) => {
+  try {
+    const { data } = await axiosInstance
+      .get(`api/sharedspaces/${url}/joinrequest`);
+
+    return data;
+  } catch (err) {
+    return Promise.reject(err);
+  }
+};
+
 export const createJoinRequest = async (
   url: string,
   RoleName: string,
