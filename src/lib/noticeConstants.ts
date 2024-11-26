e730fb95f54fb7a4d6ff2af7642e3f9dd9af21d4
melonMochiRoll <soulhwasin@gmail.com>
2024-11-26 19:51:50 +0900

    :sparkles: conflictAccountMessage 정의

--- src/lib/noticeConstants.ts
+++ src/lib/noticeConstants.ts
@@ -18,6 +18,8 @@ export const needLogin = '로그인이 필요합니다.';
 
 export const successMessage = '처리 완료되었습니다.';
 
+export const conflictAccountMessage = '이미 가입된 이메일입니다.';
+
 export const defaultToastOption: ToastOptions = {
   position: 'top-center',
   autoClose: 5000,
