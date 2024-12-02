1dd770e8b554518340ae14dac0a93eaf53e19eac
melonMochiRoll <soulhwasin@gmail.com>
2024-12-02 17:52:37 +0900

    :goal_net: 에러 핸들링

--- src/pages/SharedspacesViewPage.tsx
+++ src/pages/SharedspacesViewPage.tsx
@@ -1,24 +1,53 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import styled from '@emotion/styled';
 import CalendarContainer from 'Containers/CalendarContainer';
 import TodoContainer from 'Containers/TodoContainer';
 import SharedspaceHeader from 'Containers/SharedspaceHeader';
 import useSharedspace from 'Hooks/useSharedspace';
 import SkeletonSharedspaceHeader from 'Components/skeleton/SkeletonSharedspaceHeader';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { defaultToastOption, privateTooltip, waitingMessage } from 'Lib/noticeConstants';
 
 const SharedspacesViewPage: FC = () => {
+  const navigate = useNavigate();
   const {
     data: spaceData,
+    error,
+    errorCode,
     isLoading,
   } = useSharedspace();
+
+  useEffect(() => {
+    if (!isLoading && error) {
+      const delay = setTimeout(() => {
+        if (!error) {
+          clearTimeout(delay);
+        } else {
+          const errorMessage = errorCode === 403 ?
+            privateTooltip :
+            waitingMessage;
+
+          toast.error(errorMessage, {
+            ...defaultToastOption,
+          });
+          navigate('/login');
+        }
+      }, 1000);
+
+      return () => {
+        clearTimeout(delay);
+      };
+    }
+  }, [isLoading, error]);
   
   return (
     <Block>
       {
-        isLoading ?
-          <SkeletonSharedspaceHeader /> :
+        !isLoading && spaceData ?
           <SharedspaceHeader
-            spaceData={spaceData}/>
+            spaceData={spaceData} /> :
+          <SkeletonSharedspaceHeader />
       }
       <Main>
         <CalendarContainer />
