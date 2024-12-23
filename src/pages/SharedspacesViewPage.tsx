12703af93ba03969740536a4ebb7fd2ee5260a09
melonMochiRoll <soulhwasin@gmail.com>
2024-12-23 18:56:39 +0900

    :art: 레이아웃 구조 수정

--- src/pages/SharedspacesViewPage.tsx
+++ src/pages/SharedspacesViewPage.tsx
@@ -1,72 +1,10 @@
-import React, { FC, useEffect } from 'react';
-import styled from '@emotion/styled';
+import React, { FC } from 'react';
 import CalendarContainer from 'Containers/CalendarContainer';
-import TodoContainer from 'Containers/TodoContainer';
-import SharedspaceHeader from 'Containers/SharedspaceHeader';
-import useSharedspace from 'Hooks/useSharedspace';
-import SkeletonSharedspaceHeader from 'Components/skeleton/SkeletonSharedspaceHeader';
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import { defaultToastOption, privateTooltip, waitingMessage } from 'Lib/noticeConstants';
 
 const SharedspacesViewPage: FC = () => {
-  const navigate = useNavigate();
-  const {
-    data: spaceData,
-    error,
-    errorCode,
-    isLoading,
-  } = useSharedspace();
-
-  useEffect(() => {
-    if (!isLoading && error) {
-      const delay = setTimeout(() => {
-        if (!error) {
-          clearTimeout(delay);
-        } else {
-          const errorMessage = errorCode === 403 ?
-            privateTooltip :
-            waitingMessage;
-
-          toast.error(errorMessage, {
-            ...defaultToastOption,
-          });
-          navigate('/login');
-        }
-      }, 500);
-
-      return () => {
-        clearTimeout(delay);
-      };
-    }
-  }, [isLoading, error]);
-  
   return (
-    <Block>
-      {
-        !isLoading && spaceData ?
-          <SharedspaceHeader
-            spaceData={spaceData} /> :
-          <SkeletonSharedspaceHeader />
-      }
-      <Main>
-        <CalendarContainer />
-        <TodoContainer />
-      </Main>
-    </Block>
+    <CalendarContainer />
   );
 };
 
 export default SharedspacesViewPage;
-
-const Block = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-  background-color: var(--black);
-`;
-
-const Main = styled.main`
-  display: flex;
-  height: 95%;
-`;
